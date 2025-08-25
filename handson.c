d3983f1178a3dfe19163ba9ed9d42a8e5d201bd1
zawcompany <151438772+zawcompany@users.noreply.github.com>
2025-08-25 09:53:38 +0800

    Fixed Hands On Pertama

--- handson.c
+++ handson.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 int main(){
-int a, faktorial; 
-scanf("%d", a);
+int a; 
+printf ("Masukkan angka: ");
+scanf("%d", &a);
+
+long long faktorial = 1;
 
 if(a<0){
     printf("Faktorial tidak bisa untuk bilangan negatif");
-} else if (a=0){
+} else if (a==0){
     printf("Hasil faktorial dari 0 adalah 1");
 }else {
     for (int i =1; i<=a; i++ )
